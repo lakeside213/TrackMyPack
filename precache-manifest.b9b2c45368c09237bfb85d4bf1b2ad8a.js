72179276df39c2bbc70553c9ed3b4b83e9e65e9e
lekan <kokulekan23@gmail.com>
2019-03-30 06:19:31 +0100

    Updates

---
+++ precache-manifest.b9b2c45368c09237bfb85d4bf1b2ad8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be998590070ac3235d53",
+    "url": "/TrackMyPack/static/js/runtime~main.676b9a0d.js"
+  },
+  {
+    "revision": "520719b89a0afa0b0a4c",
+    "url": "/TrackMyPack/static/js/main.a6fcb61a.chunk.js"
+  },
+  {
+    "revision": "4e84f2f0d473eed0c3b6",
+    "url": "/TrackMyPack/static/js/2.5bc5748f.chunk.js"
+  },
+  {
+    "revision": "520719b89a0afa0b0a4c",
+    "url": "/TrackMyPack/static/css/main.8d79c04a.chunk.css"
+  },
+  {
+    "revision": "6e06bc4dd7021b8628b792acc11aeebd",
+    "url": "/TrackMyPack/index.html"
+  }
+];
