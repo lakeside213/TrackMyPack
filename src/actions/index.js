d384f3db7fa54876cb3d7c60324946a17e238b13
Lekan Koku <kokulekan23@gmail.com>
2019-04-10 10:41:28 +0200

    bug fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,8 +8,8 @@ import axios from "axios";
 
 const ROOT_URL =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:8000/"
-    : "https://trackmypack.herokuapp.com/";
+    ? "http://localhost:8000"
+    : "https://trackmypack.herokuapp.com";
 export const addPackage = (trackingNumber, packageName) => {
   return async function(dispatch) {
     try {
